540c09106e2dcf1c390fad92330e8fe71c69c606
Tonchikxx <ustikantonina@gmail.com>
2025-09-19 04:51:37 +0300

    Maze generation

---
+++ Maze/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+using Maze;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        MazeGenerator mazeGame = new();
+
+        mazeGame.RunGame();
+
+        Console.WriteLine("\nСпасибо за игру! Нажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
